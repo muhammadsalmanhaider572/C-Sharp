2d98e1a614ad698c49bd90c6ec0a89cea4a34a73
MuhammadSalmanHaider <msalmanhaider572@gmail.com>
2026-07-19 13:22:34 +0500

    IF & If-Else

--- Program.cs
+++ Program.cs
@@ -1,13 +1,21 @@
 ﻿using System;
-namespace MyApplication
+
+namespace MYApplication
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter your age : ");
-            int age = (Convert.ToInt32(Console.ReadLine()));
-            Console.WriteLine("Age is : " + age);
+            int number = 40;
+            if (number > 0 && number <= 20)
+            {
+                Console.WriteLine("Good Morning");
+            }
+            else if (number > 20 && number <= 35) {
+                Console.WriteLine("Good Afternoon");
+            }
+            else 
+            { Console.WriteLine("Good Evening");
+            }
         }
-    }
-}
+    } }
